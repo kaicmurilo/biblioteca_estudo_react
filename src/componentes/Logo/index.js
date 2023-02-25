cc928353ae9dd8d94d387fc0e95b74e679dccb11
Kaic Nunes <kaic.nunes@MASCARENHAS.CORP>
2023-02-25 12:05:15 -0400

    alteracao de nome e logo

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,4 +1,5 @@
 import logo from '../../imagens/logo.svg'
+import logo2 from '../../imagens/logo.png'
 import styled from 'styled-components';
 
 const LogoContainer = styled.div`
@@ -6,14 +7,15 @@ const LogoContainer = styled.div`
     font-size: 30px;
 `
 const ImgContainer = styled.img`
+width: 12%;
 margin-right: 10px;
 `
 
 function Logo() {
     return (
         <LogoContainer>
-            <ImgContainer src={logo} alt='Logo Alura Books'></ImgContainer>
-            <p><strong>Alura</strong>Books</p>
+            <ImgContainer src={logo2} alt='Logo Estante Books'></ImgContainer>
+            <p><strong>Estante</strong>Books</p>
         </LogoContainer>
     )
 }
